cff13777c78dda241f82910ab38c8a4d02fdd147
Hao Xu <hxuu0069@student.monash.edu>
2023-05-16 10:55:12 +1000

    slightly change words

--- src/components/occupations/JobSkillMiniGame.tsx
+++ src/components/occupations/JobSkillMiniGame.tsx
@@ -263,7 +263,7 @@ export default function JobSkillMiniGame({
       </Flex>
       {isShowAnswer && (
         <Flex direction={{ base: "column", md: "row" }}>
-          <Heading>Are you satisfied with this result?</Heading>
+          <Heading>Would you like to know more about this occupation?</Heading>
           <Spacer />
           <ButtonGroup colorScheme="purple">
             <Button onClick={onOpen}>Yes</Button>
